60e736ef05e160a1ee5ffc4aa5dbf6d331f68700
leojojo <setsunal.fujita@gmail.com>
2020-09-09 03:45:07 +0900

    iterator

--- fizzbuzz.pl
+++ fizzbuzz.pl
@@ -2,6 +2,8 @@ use warnings;
 use strict;
 use feature qw(say);
 
+sub Iterator (&) { return $_[0] }
+
 
 
 main();
@@ -10,7 +12,12 @@ sub main {
   print "FizzBuzzの最大値: ";
   my $input = <STDIN>;
 
-  fizzbuzz($input);
+  # fizzbuzz($input);
+
+  my $iterator = iter_fizzbuzz($input);
+  while (my $val = $iterator->()) {
+    say $val;
+  }
 }
 
 sub fizzbuzz {
@@ -28,5 +35,16 @@ sub fizzbuzz {
     } else {
       say $_;
     }
+  }
+}
+
+sub iter_fizzbuzz {
+  my ($max) = @_;
+  my $current;
+
+  return Iterator {
+    $current++;
+    return undef if $current > $max;
+    return $current % 15 == 0 ? "FizzBuzz" : $current % 3 == 0 ? "Fizz" : $current % 5 == 0 ? "Buzz" : $current;
   }
 }
